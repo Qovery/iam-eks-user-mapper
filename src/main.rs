54de57e886373f0787519feb65df00ab1758f55c
Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>
2024-11-07 21:16:00 +0100

    feat: Allow aws access key authentication (#36)

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use crate::kubernetes::{
     IamArn, IamUserName, KubernetesGroupName, KubernetesRole, KubernetesService, KubernetesUser,
     SyncedBy,
 };
-use clap::Parser;
+use clap::{ArgGroup, Parser};
+use config::CredentialsMode;
 use std::collections::{HashMap, HashSet};
 use std::time::Duration;
 use tokio::{task, time};
@@ -20,13 +21,24 @@ use tracing_subscriber::{prelude::*, EnvFilter, FmtSubscriber};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[command(group(
+    ArgGroup::new("aws_credentials")
+        .args(&["aws_role_arn", "aws_access_key_id"])
+        .required(true)
+))]
 struct Args {
     /// Service account name to be used, e.q: my-service-account
     #[arg(short = 's', long, env, required = true)]
     pub service_account_name: String,
     /// AWS role ARN to be used, e.q: arn:aws:iam::12345678910:role/my-role
-    #[arg(short = 'R', long, env, required = true)]
-    pub aws_role_arn: String,
+    #[arg(short = 'R', long, env, conflicts_with_all = &["aws_access_key_id", "aws_secret_access_key"])]
+    pub aws_role_arn: Option<String>,
+    /// AWS access key ID to be used
+    #[arg(short = 'a', long, env, requires = "aws_secret_access_key")]
+    pub aws_access_key_id: Option<String>,
+    /// AWS secret access key to be used
+    #[arg(short = 'k', long, env, requires = "aws_access_key_id")]
+    pub aws_secret_access_key: Option<String>,
     /// AWS default region to be used, e.q: eu-west-3
     #[arg(short = 'r', long, env, required = true)]
     pub aws_default_region: String,
@@ -34,7 +46,7 @@ struct Args {
     #[arg(short = 'i', long, env, default_value_t = 60)]
     pub refresh_interval_seconds: u64,
     /// Activate group user sync (requires `iam_k8s_groups` to be set)
-    #[clap(long, env, required = true, default_value_t = false)]
+    #[clap(long, env, required = false, default_value_t = false)]
     pub enable_group_user_sync: bool,
     /// IAM groups to be mapped into Kubernetes, e.q: Admins->system:masters
     ///
@@ -44,7 +56,7 @@ struct Args {
     #[clap(short = 'g', long, env, value_parser, num_args = 1.., value_delimiter = ',', required = false)]
     pub iam_k8s_groups: Vec<String>,
     /// Activate SSO on the cluster (requires `iam_sso_role_arn` to be set)
-    #[clap(long, env, required = true, default_value_t = false)]
+    #[clap(long, env, default_value_t = false, required = false)]
     pub enable_sso: bool,
     /// IAM SSO role arn
     #[clap(long, env, value_delimiter = ',', required = false)]
@@ -113,6 +125,8 @@ async fn sync_iam_eks_users_and_roles(
                     underlying_error: e.into(),
                 })?;
 
+            info!("Found {} users in IAM groups", iam_users.len());
+
             Some(HashSet::from_iter(iam_users.iter().map(|u| {
                 KubernetesUser::new(
                     IamUserName::new(&u.user_name.to_string()),
@@ -164,12 +178,29 @@ async fn main() -> Result<(), errors::Error> {
 
     let args = Args::parse();
 
+    let credentials_mode = if let Some(aws_role_arn) = &args.aws_role_arn {
+        CredentialsMode::RoleBased {
+            _aws_role_arn: aws_role_arn.clone(),
+        }
+    } else if let (Some(aws_access_key_id), Some(aws_secret_access_key)) =
+        (&args.aws_access_key_id, &args.aws_secret_access_key)
+    {
+        CredentialsMode::AccessKeyBased {
+            _aws_access_key_id: aws_access_key_id.clone(),
+            _aws_secret_access_key: aws_secret_access_key.clone(),
+        }
+    } else {
+        panic!("Bad configuration");
+    };
+
+    let credentials = Credentials::new(
+        args.aws_default_region,
+        args.service_account_name,
+        credentials_mode,
+    );
+
     let config = config::Config::new(
-        Credentials::new(
-            args.aws_default_region,
-            args.service_account_name,
-            args.aws_role_arn,
-        ),
+        credentials,
         Duration::from_secs(args.refresh_interval_seconds),
         args.enable_group_user_sync,
         args.iam_k8s_groups,
